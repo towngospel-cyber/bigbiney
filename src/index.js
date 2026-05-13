32ba17541de962cba7276b899d72d7aca151dab9
towngospel-cyber <towngospel@gmail.com>
2026-05-13 06:49:44 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+document.body.style.margin = '0';
+document.body.style.padding = '0';
+
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(<App />);
+} else {
+  console.error('Root element not found');
+}
