8a11e621f710b9c958f20dd6506ef45719fd38dc
towngospel-cyber <towngospel@gmail.com>
2026-05-13 06:37:02 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import PrintingPressSystem from './App';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <PrintingPressSystem />
+    <App />
   </React.StrictMode>
 );
